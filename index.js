c6d24e6977003d139cb19a6c6d4dba85adb592aa
ngotuanviet <ngotuanviet9@gmail.com>
2025-07-11 18:36:00 +0700

    layout admin

--- index.js
+++ index.js
@@ -4,12 +4,15 @@ const route = require('./routes/client/index.routes');
 const routeAdmin = require('./routes/admin/index.routes');
 
 const database = require('./config/database');
+const system = require('./config/system');
 
 const app = express();
 require('dotenv').config();
 database.connect();
 const port = process.env.PORT;
 
+// var locals app 
+app.locals.prefixAdmin = system.prefixAdmin;
 app.use(express.static("public"));
 app.set("views", "./views");
 app.set("view engine", "pug");
--- routes/admin/dashboard.routes.js
+++ routes/admin/dashboard.routes.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const systemConfig = require("../../config/system");
 const router = express.Router();
 const controller = require("../../controllers/admin/dashbroad.controller")
 router.get("/", controller.dashboard)
