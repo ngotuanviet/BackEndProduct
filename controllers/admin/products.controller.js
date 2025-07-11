272ce5c937c2b14481664312351bf62578ede975
ngotuanviet <ngotuanviet9@gmail.com>
2025-07-11 21:05:47 +0700

    load product admin

--- controllers/admin/products.controller.js
+++ controllers/admin/products.controller.js
@@ -1,7 +1,15 @@
+const Product = require("../../models/Product.model")
+
 // [Get] /admin/products
-const index = (req, res) => {
+const index = async (req, res) => {
+    const products = await Product.find({
+        deleted: 'false'
+    })
+    console.log(products);
+
     res.render('admin/pages/products/index', {
-        title: "Trang quản lý sản phẩm"
+        title: "Trang quản lý sản phẩm",
+        products
     })
 }
 module.exports = {
