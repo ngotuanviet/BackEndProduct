e46057f3618976069488bcf65daba284b014b147
ngotuanviet <ngotuanviet9@gmail.com>
2025-07-13 09:33:41 +0700

    pagination

--- controllers/admin/products.controller.js
+++ controllers/admin/products.controller.js
@@ -1,7 +1,7 @@
 const Product = require("../../models/Product.model")
 const filtersStatusHelper = require("../../helper/filtersStatus")
 const searchHelper = require("../../helper/search")
-
+const paginationHelper = require("../../helper/pagination")
 // [Get] /admin/products
 const index = async (req, res) => {
     const { status, keyword, page } = req.query;
@@ -20,28 +20,32 @@ const index = async (req, res) => {
     }
 
     // pagination
-    let objectPanination = {
+    const countDocuments = await Product.countDocuments(find)
+    let objectPanination = await paginationHelper({
         limitItems: 4,
         currentPage: 1,
-        skip: 4
-    }
 
-    if (page) {
-        let pageInt = parseInt(page)
-        if (isNaN(pageInt)) {
-            objectPanination.currentPage = 1
-        } else if (page < 1) {
-            objectPanination.currentPage = 1
-        } else {
-            objectPanination.currentPage = pageInt
-            // (trang hiện tại - 1) * Số lượng xuất hiện phần tử mỗi trang
-            objectPanination.skip = (objectPanination.currentPage - 1) * objectPanination.limitItems
-        }
 
-    }
-    const countDocuments = await Product.countDocuments(find)
-    const totalPages = Math.ceil(countDocuments / objectPanination.limitItems)
-    objectPanination.totalPages = totalPages
+    }, page, countDocuments)
+    console.log(objectPanination);
+
+
+    // if (page) {
+    //     let pageInt = parseInt(page)
+    //     if (isNaN(pageInt)) {
+    //         objectPanination.currentPage = 1
+    //     } else if (page < 1) {
+    //         objectPanination.currentPage = 1
+    //     } else {
+    //         objectPanination.currentPage = pageInt
+    //         // (trang hiện tại - 1) * Số lượng xuất hiện phần tử mỗi trang
+    //         objectPanination.skip = (objectPanination.currentPage - 1) * objectPanination.limitItems
+    //     }
+
+    // }
+    // const countDocuments = await Product.countDocuments(find)
+    // const totalPages = Math.ceil(countDocuments / objectPanination.limitItems)
+    // objectPanination.totalPages = totalPages
     // End pagination
     const products = await Product.find(find).limit(objectPanination.limitItems).skip(objectPanination.skip)
 
---
+++ helper/pagination.js
@@ -0,0 +1,20 @@
+module.exports = async (objectPanination, page, countDocuments) => {
+    if (page) {
+        let pageInt = parseInt(page)
+        if (isNaN(pageInt)) {
+            objectPanination.currentPage = 1
+        } else if (page < 1) {
+            objectPanination.currentPage = 1
+        } else {
+            objectPanination.currentPage = pageInt
+            // (trang hiện tại - 1) * Số lượng xuất hiện phần tử mỗi trang
+            objectPanination.skip = (objectPanination.currentPage - 1) * objectPanination.limitItems
+        }
+
+    }
+
+    const totalPages = Math.ceil(countDocuments / objectPanination.limitItems)
+    objectPanination.totalPages = totalPages
+    return objectPanination
+
+}
