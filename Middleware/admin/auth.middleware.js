2d2b3608611aa8d04074c2d8cf6a026054694bfb
ngotuanviet <ngotuanviet9@gmail.com>
2025-07-24 19:09:50 +0700

    authorization

--- Middleware/admin/auth.middleware.js
+++ Middleware/admin/auth.middleware.js
@@ -1,14 +1,19 @@
 const SytemConfig = require("../../config/system");
 const Accounts = require("../../models/Accounts.model");
+const Role = require("../../models/Roles.model");
 const requireAuth = async (req, res, next) => {
     try {
-        const users = await Accounts.findOne({ token: req.cookies.token, deleted: false });
+        const users = await Accounts.findOne({ token: req.cookies.token, deleted: false }).select("-password");
         if (!req.cookies.token) {
             res.redirect(`${SytemConfig.prefixAdmin}/auth/login`);
         } else if (!users) {
             res.redirect(`${SytemConfig.prefixAdmin}/auth/login`);
 
         } else {
+            const roles = await Role.findOne({ _id: users.role_id, deleted: false }).select("title permissions");
+            res.locals.user = users;
+            res.locals.rolesUser = roles;
+
             next()
         }
     } catch (error) {
@@ -16,8 +21,6 @@ const requireAuth = async (req, res, next) => {
 
         res.redirect(`${SytemConfig.prefixAdmin}/auth/login`);
     }
-
-
 }
 module.exports = {
     requireAuth
