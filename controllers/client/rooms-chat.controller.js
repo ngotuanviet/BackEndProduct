782110c3ed69e0d9f7bd4ab33e8187a2be8ac5a2
ngotuanviet <ngotuanviet9@gmail.com>
2025-09-02 17:35:25 +0700

    Group chat

--- controllers/client/rooms-chat.controller.js
+++ controllers/client/rooms-chat.controller.js
@@ -1,9 +1,47 @@
+const RoomChat = require("../../models/rooms-chat.model");
+const Users = require("../../models/Users.model");
+
 const index = (req, res) => {
   res.render("client/pages/rooms-chat", {
-    title: "Chat Room",
+    title: "Phòng chat",
   });
 };
+const create = async (req, res) => {
+  const users = res.locals.user.friendsList;
+  const friendsID = users.map((item) => item.user_ID);
+  const infosFriends = await Users.find({
+    _id: { $in: friendsID },
+    deleted: false,
+  }).select(" fullName avatar");
 
+  res.render("client/pages/rooms-chat/create", {
+    title: "Tạo phòng chat",
+    friends: infosFriends,
+  });
+};
+const createPost = async (req, res) => {
+  const { title, usersId } = req.body;
+
+  const newRoom = {
+    title,
+    typeRoom: "Group",
+    users: [],
+  };
+  if (Array.isArray(usersId)) {
+    for (const userID of usersId) {
+      newRoom.users.push({ userID, role: "member" });
+    }
+  } else {
+    newRoom.users.push({ userID: usersId, role: "member" });
+  }
+
+  newRoom.users.push({ userID: res.locals.user.id, role: "superAdmin" });
+  const chatRoom = new RoomChat(newRoom);
+  await chatRoom.save();
+  res.redirect(`/chat/${chatRoom.id}`);
+};
 module.exports = {
   index,
+  create,
+  createPost,
 };
--- routes/client/room-chat.routes.js
+++ routes/client/room-chat.routes.js
@@ -2,4 +2,6 @@ const e = require("express");
 const Controller = require("../../controllers/client/rooms-chat.controller");
 const router = e.Router();
 router.get("/", Controller.index);
+router.get("/create", Controller.create);
+router.post("/create", Controller.createPost);
 module.exports = router;
